1ee8a6073a27782d035b0ce8ed68d76c79666aaf
김정수 <kgs1595123456@gmail.com>
2018-05-23 16:52:39 +0900

    기본 시스템 업로드

---
+++ TeamInvaders/Assets/Script/System/GameData.cs
@@ -0,0 +1,27 @@
+﻿
+public enum Difficulty
+{
+    Easy,
+    Normal,
+    Hard,
+}
+public enum ShipType
+{
+    Red,
+    Yello,
+    Green,
+}
+[System.Serializable]
+public class GameData : Singleton<GameData>
+{
+    protected GameData() { } // 싱글톤이기 때문에 생성자는 사용할 수 없다.
+    public int PlayerDamage;
+    public int PlayerHaveGold;
+    public Difficulty Difficulte;
+    public ShipType PlayerShipType;
+    public bool PowerItem;
+    public bool LifeItem;
+    public bool UltimateItem;
+    public bool MagnaticItem;
+
+}
---
+++ TeamInvaders/Assets/Script/System/GameDataController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class GameDataController : MonoBehaviour 
+{
+    /// <summary>
+    /// 게임데이터 접근시 아래 프로퍼티를 사용하여 접근하세요!
+    /// 직접 GameData.cs를 함부로 만지 마세요
+    /// 데이터 변경시 변경되었다는 Log를 Console창에 출력합니다.
+    /// </summary>
+    public int PlayerDamage { get { return GameData.Instance.PlayerDamage; } set { GameData.Instance.PlayerDamage = value; ChangeDatas(); } }
+    public int PlayerHaveGold { get { return GameData.Instance.PlayerHaveGold; } set { GameData.Instance.PlayerHaveGold = value; ChangeDatas(); } } 
+    public Difficulty Difficulte { get { return GameData.Instance.Difficulte; } set { GameData.Instance.Difficulte = value; ChangeDatas(); } }
+    public ShipType PlayerShipType { get { return GameData.Instance.PlayerShipType; } set{ GameData.Instance.PlayerShipType = value; ChangeDatas(); } }
+    public bool PowerItem{ get { return GameData.Instance.PowerItem; } set { GameData.Instance.PowerItem = value; ChangeDatas(); } }
+    public bool LifeItem{get { return GameData.Instance.LifeItem; } set { GameData.Instance.LifeItem = value; ChangeDatas(); } }  
+    public bool UltimateItem { get { return GameData.Instance.UltimateItem; } set { GameData.Instance.UltimateItem = value; ChangeDatas(); } }
+    public bool MagnaticItem { get { return GameData.Instance.MagnaticItem; } set { GameData.Instance.MagnaticItem = value; ChangeDatas(); } }
+
+    public void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void ChangeDatas()
+    {
+        Debug.Log("값 변경됨");
+    }
+  
+
+}
