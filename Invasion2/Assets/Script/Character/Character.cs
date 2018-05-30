ce158f8f92da78f4197268096de993ca2883822b
박상원 <psw14com@gmail.com>
2018-05-30 16:05:27 +0900

    이동 껍데기

---
+++ Invasion2/Assets/Script/Character/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 캐릭터 이동 공통부분 전반 담당
+/// Player와 Enemy에서 Character를 상속받아 사용
+/// </summary>
+public class Character : MonoBehaviour
+{
+    public float moveSpeed;
+    public int damage;
+    public int life;
+
+    public virtual void Move(Vector3 direction)
+    {
+
+    }
+    public virtual void Attack()
+    {
+
+    }
+    public virtual void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
---
+++ Invasion2/Assets/Script/Character/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Character를 상속받는다.
+/// enum은 다른 클래스에서 쉽게 불러올 수 있도록
+/// 클래스 밖에 선언하였음.
+/// </summary>
+public enum MainAttack
+{
+    Sector,
+    Wedge,
+    Straight,
+}
+
+public enum SubAttack
+{
+    Straight,
+    Guaidance,
+    Shild,
+}
+
+public class Player : Character
+{
+    public override void Move(Vector3 direction)
+    {
+        base.Move(direction);
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
