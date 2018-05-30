16687415c6bf3860ebe735a7d0401cafee8f8227
최대원 <chdw2438@gmail.com>
2018-05-30 17:13:40 +0900

    StageManager 및 Factory

--- Invasion2/Assets/Script/Manager/StageManager.cs
+++ Invasion2/Assets/Script/Manager/StageManager.cs
@@ -1,14 +1,61 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Difficult
+{
+    Easy,
+    Normal,
+    Hard,
+}
+
 public class StageManager : Singleton<StageManager> {
 
-    protected StageManager() { }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected StageManager()
+    {
+        CurrentStage = 0;
+    }
+
+    public Difficult difficult;
+    public int CurrentStage;
+    public List<Enemy> EnemyList;
+    public EnemyFactory Factory;
+
+    const int MaxStage = 3;
+
+    public void Spawn(Enemy enemy)
+    {
+        Debug.Log("Enemy Spawn");
+        Enemy SpwanEnemy =  Factory.CreateEnemy(enemy);
+        EnemyList.Add(SpwanEnemy);
+    }
+
+    public void NextStage()
+    {
+        if(CurrentStage < MaxStage)
+        {
+            Debug.Log("Call NextStage");
+            StopCoroutine(StageCoroutine(CurrentStage));
+            StartCoroutine(StageCoroutine(CurrentStage++));
+        }
+    }
+
+    public void SetDifficulty(Difficult difficult)
+    {
+        Debug.Log("Set Difficulty");
+        this.difficult = difficult;
+    }
+
+    public Difficult GetDifficulty()
+    {
+        Debug.Log("Get Difficulty");
+        return difficult;
+    }
+
+    IEnumerator StageCoroutine(int stageLevel)
+    {
+        Debug.Log("Call StageCoroutine");
+        yield break;
+    }
 }
---
+++ Invasion2/Assets/Script/StageSystem/EnemyFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    public Enemy CreateEnemy(Enemy EnemyPrefab)
+    {
+        return Instantiate(EnemyPrefab);
+    }
+}
