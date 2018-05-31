52d3828488adb5d02db1071f67a35d10d388a0a9
최대원 <chdw2438@gmail.com>
2018-05-31 16:55:46 +0900

    PIET-19 스테이지 수정중

--- Invasion2/Assets/Script/StageSystem/EnemyFactory.cs
+++ Invasion2/Assets/Script/StageSystem/EnemyFactory.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 에너미 생성하는 팩토리
+/// </summary>
 public class EnemyFactory : MonoBehaviour
 {
     public Enemy CreateEnemy(Enemy EnemyPrefab)
     {
+        Debug.Log("call CreateEnemy");
         return Instantiate(EnemyPrefab);
     }
 }
