f8d0e90b15a512bbc0245be0ff6be90f7ccddb22
박상원 <psw14com@gmail.com>
2018-05-31 16:02:18 +0900

    PIET-23 , PIET-22 , PIET-27 _이동관련 구현

--- Invasion2/Assets/Script/Character/Character.cs
+++ Invasion2/Assets/Script/Character/Character.cs
@@ -6,15 +6,69 @@ using UnityEngine;
 /// 캐릭터 이동 공통부분 전반 담당
 /// Player와 Enemy에서 Character를 상속받아 사용
 /// </summary>
+public enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    LEFTUP,
+    RIGHTUP,
+    LEFTDOWN,
+    RIGHTDOWN,
+}
 public class Character : MonoBehaviour
 {
     public float moveSpeed;
     public int damage;
     public int life;
+    public Vector3 pos;
 
-    public virtual void Move(Vector3 direction)
+    new Rigidbody rigidbody;
+
+    private void Start()
     {
+        rigidbody = GameObject.Find("Player").GetComponent<Rigidbody>();
+        pos = rigidbody.position;
+    }
 
+    public Direction direction;
+    public virtual void Move(Direction direction)
+    {
+        Vector3 target;
+        Vector3 targetDirection;
+        switch (direction)
+        {
+            case Direction.UP:
+                target = new Vector3(0.0f, 10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.DOWN:
+                pos = rigidbody.position;
+                target = new Vector3(0.0f, -10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.LEFT:
+                pos = rigidbody.position;
+                target = new Vector3(-10.0f, 0.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.RIGHT:
+                pos = rigidbody.position;
+                target = new Vector3(10.0f, 0.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            default:
+                break;
+        }
     }
     public virtual void Attack()
     {
--- Invasion2/Assets/Script/Character/Enemy/Enemy.cs
+++ Invasion2/Assets/Script/Character/Enemy/Enemy.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 /// Character를 상속받는다.
 /// enum은 다른 클래스에서 사용하기 편하도록
 /// 클래스 밖으로 빼놓았음.
+/// Move는 재정의하여 Enemy에 맞도록
+/// 내용을 채워넣었음.
 /// </summary>
 public enum EnemyType
 {
@@ -14,19 +16,86 @@ public enum EnemyType
 
 public class Enemy : Character
 {
-    Vector3 direction;
-
     public int giveScore;
     public int giveMaxGold;
+
+    new Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        pos = rigidbody.position;
+    }
 
     private void FixedUpdate()
     {
         Move(direction);
     }
 
-    public override void Move(Vector3 direction)
+    public override void Move(Direction direction)
     {
-        base.Move(direction);
+        Vector3 target;
+        Vector3 targetDirection;
+        switch (direction)
+        {
+            case Direction.UP:
+                target = new Vector3(0.0f, 10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.DOWN:
+                pos = rigidbody.position;
+                target = new Vector3(0.0f, -10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.LEFT:
+                pos = rigidbody.position;
+                target = new Vector3(-10.0f, 0.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.RIGHT:
+                pos = rigidbody.position;
+                target = new Vector3(10.0f, 0.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.LEFTUP:
+                pos = rigidbody.position;
+                target = new Vector3(-10.0f, 10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.RIGHTUP:
+                pos = rigidbody.position;
+                target = new Vector3(10.0f, 10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.LEFTDOWN:
+                pos = rigidbody.position;
+                target = new Vector3(-10.0f, -10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            case Direction.RIGHTDOWN:
+                pos = rigidbody.position;
+                target = new Vector3(10.0f, -10.0f, 0.0f);
+                targetDirection = (target - pos).normalized;
+                rigidbody.velocity = targetDirection * moveSpeed;
+                Attack();
+                break;
+            default:
+                break;
+        }
     }
 
     public override void Attack()
