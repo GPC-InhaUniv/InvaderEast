c2aac56d698383616470a2d7b7e74b6ed6371b7f
최대원 <chdw2438@gmail.com>
2018-05-31 17:35:12 +0900

    PIET-19 180531 스테이지 시스템 수정

--- Invasion2/Assets/Script/Manager/StageManager.cs
+++ Invasion2/Assets/Script/Manager/StageManager.cs
@@ -13,7 +13,7 @@ public enum Difficult
 /// <summary>
 /// 중재자를 통해 스테이지 시작 요청 받으면 코루틴 시작
 /// 코루틴 내부에서 스테이지에 맞게 적 생성
-/// List에서 에너미 관리
+/// List를 통해 에너미 관리
 /// </summary>
 
 public class StageManager : Singleton<StageManager> {
@@ -29,8 +29,6 @@ public class StageManager : Singleton<StageManager> {
     public EnemyFactory Factory;
     GameMediator gameMediator;
 
-    public Enemy enemy;
-
     const int MaxStage = 3;
 
     private void Start()
@@ -42,10 +40,11 @@ public class StageManager : Singleton<StageManager> {
     {
         if(enemy.tag == "Enemy")
         {
-            Debug.Log("Enemy Spawn");
             Enemy SpwanEnemy = Factory.CreateEnemy(enemy);
             EnemyList.Add(SpwanEnemy);
+            Debug.Log("Enemy Spawn 완료");
         }
+        else Debug.Log("Spawn() 메서드의 tag 불일치!");
     }
 
     public void RemoveAllEnemy()
@@ -55,6 +54,7 @@ public class StageManager : Singleton<StageManager> {
             Destroy(item.gameObject);
         }
         EnemyList.Clear();
+        Debug.Log("RemoveAllEnemy 완료");
     }
 
     public void RemoveEnemy(Enemy enemy)
@@ -62,37 +62,48 @@ public class StageManager : Singleton<StageManager> {
         Debug.Log("Enemy remove");
         if (enemy.tag == "Enemy")
         {
-           
             EnemyList.Remove(enemy);
             Destroy(enemy.gameObject);
-        } 
+        }
+        else Debug.Log("RemoveEnemy 메서드의 tag 불일치!");
     }
 
     public void NextStage()
     {
-        if(CurrentStage < MaxStage)
+        
+        if (CurrentStage == 0)
         {
-            Debug.Log("Call NextStage : " + CurrentStage);
+            
+            StartCoroutine(StageCoroutine(CurrentStage));
+            CurrentStage++;
+        }
+        else if (CurrentStage <= MaxStage)
+        {
             StopCoroutine(StageCoroutine(CurrentStage));
             StartCoroutine(StageCoroutine(CurrentStage++));
         }
+        else
+        {
+            Debug.Log("다음 스테이지 없음");
+        }
+        
     }
 
     public void SetDifficulty(Difficult difficult)
     {
-        Debug.Log("Set Difficulty");
+        Debug.Log("Set Difficulty : " + difficult);
         this.difficult = difficult;
     }
 
     public Difficult GetDifficulty()
     {
-        Debug.Log("Get Difficulty");
+        Debug.Log("Get Difficulty : " + difficult);
         return difficult;
     }
 
     IEnumerator StageCoroutine(int stageLevel)
     {
-        Debug.Log("Call StageCoroutine");
+        Debug.Log("스테이지 " + stageLevel + " 시작");
         yield break;
     }
 }
