6b6a2eb838171da7efc1e2f31c19abf744d689e8
최대원 <chdw2438@gmail.com>
2018-06-01 16:59:38 +0900

    PIET-29 대충 완료

---
+++ Invasion2/Assets/Script/UI/StartSceneCtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneCtrl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject HelpPanel;
+    [SerializeField]
+    GameObject RankPanel;
+
+    public void OnClickGameStart()
+    {
+
+    }
+
+    public void OnClickHelp()
+    {
+        HelpPanel.SetActive(true);
+    }
+
+    public void OnClickRank()
+    {
+        RankPanel.SetActive(true);
+    }
+
+    public void OnClickExit()
+    {
+
+    }
+
+    public void OnClickByCloseHelpPanel()
+    {
+        HelpPanel.SetActive(false);
+    }
+
+    public void OnClickByCloseRankPanel()
+    {
+        RankPanel.SetActive(false);
+    }
+}
