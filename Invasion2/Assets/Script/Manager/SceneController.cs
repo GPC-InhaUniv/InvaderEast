7218915e1de0ee2e61e37c3cc40cf82a825301a2
김정수 <kgs1595123456@gmail.com>
2018-06-05 15:39:27 +0900

    PIET-42 씬 매니저 v 0.1

--- Invasion2/Assets/Script/Manager/SceneController.cs
+++ Invasion2/Assets/Script/Manager/SceneController.cs
@@ -2,8 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Runtime.Serialization;
+
+using UnityEngine.SceneManagement;
+public enum SceneState
+{
+    Title,
+    CharacterSelect,
+    Store,
+    Battle,
+    
+
+}
+
 public class SceneController : Singleton<SceneController> {
 
     protected SceneController() { }
+
+    public void ChangeScene(SceneState state)
+    {
+        switch (state)
+        {
+            case SceneState.Title:
+                break;
+            case SceneState.CharacterSelect:
+                break;
+            case SceneState.Store:
+                break;
+            case SceneState.Battle:
+                break;
+            default:
+                break;
+        }
+    }
 }
