ad28385f90b1d8f7eda8e26242ad9986e2c0c495
김정수 <kgs1595123456@gmail.com>
2018-06-07 15:21:29 +0900

    PIET-40 중재자 편집

--- Invasion2/Assets/Script/Manager/SceneController.cs
+++ Invasion2/Assets/Script/Manager/SceneController.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 public enum SceneState
 {
