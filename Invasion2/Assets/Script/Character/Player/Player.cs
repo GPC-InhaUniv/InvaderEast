2fb6f9d0c6277799b1598d5cb0bba4fffbb61a37
박상원 <psw14com@gmail.com>
2018-06-08 17:43:16 +0900

    PIET-22 _ Attack 메서드에 공격 테스트용 스크립트 추가

--- Invasion2/Assets/Script/Character/Player/Player.cs
+++ Invasion2/Assets/Script/Character/Player/Player.cs
@@ -10,6 +10,9 @@ public class Player : Character
     ISubAttackable subAttack;
     Guaidance guaidance;
     int attackCount;
+    float fireRate = 0.2f;
+    float timeRate = 0.0f;
+    bool fire = false;
 
     private void Start()
     {
@@ -33,15 +36,23 @@ public class Player : Character
     {
         //공격 관련 프리펩은 나중에 하나의
         //오브젝트에 모아놓기로 결정
-        mainAttack.Attack(damage);
+        if(timeRate<fireRate)
+        {
+            timeRate += Time.deltaTime;
+            return;
+        }
+        else if(timeRate>=fireRate)
+        {
+            mainAttack.Attack(damage);
+            timeRate = 0.0f;
+            attackCount++;
+        }
+            
+        
         if(attackCount==3)
         {
             subAttack.Attack(damage);
             attackCount = 0;
-        }
-        else if(attackCount<3)
-        {
-            attackCount++;
         }
     }
 
