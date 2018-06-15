a3abef59077ca84223e1c9b25d0504191a23a03d
김지섭 <slaytude@gmail.com>
2018-06-15 13:10:26 +0900

    PIET-24 일반 몬스터 및 메뉴 UI

---
+++ Invasion2/Assets/Script/Character/Enemy/IEnemyShotPattern.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 담당자 : 김지섭
+
+// 브릿지 패턴 중에 Implementor 부분입니다
+
+interface IEnemyShotPattern
+{
+    IEnumerator ShootStraight();
+    IEnumerator ShootFiveTimes();
+    IEnumerator ShootInductionBullet();
+    IEnumerator ShootTwoinNumber();
+    IEnumerator ShootFanwise();
+
+}
