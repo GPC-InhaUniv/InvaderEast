1163ba53364cc7dab5d4cde98c04c0497d3403c9
이재환 <ok17jh@gmail.com>
2018-06-21 16:13:50 +0900

    사용하지 않는 메서드 명령줄 주석처리

--- Invasion2/Assets/Script/GameController.cs
+++ Invasion2/Assets/Script/GameController.cs
@@ -24,7 +24,7 @@ public class GameController : MonoBehaviour {
 
         //PoolManager.Instance.SetQueue();
         
-        StageManager.Instance.NextStage();
+        //StageManager.Instance.NextStage();
 
     }
 	
