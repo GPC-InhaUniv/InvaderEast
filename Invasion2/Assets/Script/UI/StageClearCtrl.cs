bc371ab417cfc2939fb8e30685c131959b7bda27
최대원 <chdw2438@gmail.com>
2018-06-22 16:57:43 +0900

    PIET-32 클리어 메뉴 UI에 스코어 추가

--- Invasion2/Assets/Script/UI/StageClearCtrl.cs
+++ Invasion2/Assets/Script/UI/StageClearCtrl.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 작성자 : 최대원
 /// 스테이지 클리어 시 나타나는 패널의 컨트롤러
 /// </summary>
 public class StageClearCtrl : MonoBehaviour {
+
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    int addScore = 7;
+
+    int currentScore;
+    int maxScore;
 
     public void OnClickReturnToTitle()
     {
@@ -22,4 +31,20 @@ public class StageClearCtrl : MonoBehaviour {
         StageManager.Instance.NextStage();
     }
 
+    private void OnEnable()
+    {
+        maxScore = InputManager.Instance.ReadCurrentScore();
+    }
+
+    private void Update()
+    {
+        if (currentScore < maxScore)
+        {
+            currentScore += addScore;
+        }
+        else if (currentScore > maxScore)
+        {
+            currentScore = maxScore;
+        }
+    }
 }
