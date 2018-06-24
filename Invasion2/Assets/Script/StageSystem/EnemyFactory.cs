b3b9ec896d88fe820cb9c1d5ed612d7aa8ed1d11
최대원(집컴) <chdw2438@gmail.com>
2018-06-24 19:03:00 +0900

    코드 정리

--- Invasion2/Assets/Script/StageSystem/EnemyFactory.cs
+++ Invasion2/Assets/Script/StageSystem/EnemyFactory.cs
@@ -5,21 +5,12 @@ using UnityEngine;
 /// <summary>
 /// 담당자 : 최대원
 /// 에너미를 생성하는 팩토리
-/// PoolManager와 연동해야 함
 /// </summary>
 public class EnemyFactory
 {
-    GameMediator gameMediator;
-
-    /*public EnemyFactory(GameMediator gameMediator)
-    {
-        this.gameMediator = gameMediator;
-    }*/
-
     public GameObject CreateEnemy()
     {
         GameObject enemy = PoolManager.Instance.GetEnemyObject();
-        
         return enemy;
     }
 }
